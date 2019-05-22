6690201fc0dba88b8f0dc8fd48866a0753f9136e
IvanCl4udio <ivan.claudio@gmail.com>
2019-05-22 06:24:37 -0300

    Created Spring initializr project

---
+++ src/test/java/net/ivancl4udio/apps/oauth2authenticator/Oauth2AuthenticatorApplicationTests.java
@@ -0,0 +1,16 @@
+package net.ivancl4udio.apps.oauth2authenticator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2AuthenticatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
